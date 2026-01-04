aa16cc57a2d2f01d4407dfd30de8aeb425de1a81
venkatesh.v <venkatesh.v@anthology.com>
2026-01-04 12:08:40 +0530

    Handled consumer exceptions of kafka events to push it to DLT

--- src/main/java/org/project/notificationservice/config/KafkaConsumerConfig.java
+++ src/main/java/org/project/notificationservice/config/KafkaConsumerConfig.java
@@ -1,15 +1,22 @@
 package org.project.notificationservice.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.ByteArrayDeserializer;
+import org.apache.kafka.common.serialization.ByteArraySerializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
 import tools.jackson.databind.ObjectMapper;
 import tools.jackson.databind.json.JsonMapper;
 
@@ -30,6 +37,23 @@ public class KafkaConsumerConfig {
     }
 
     @Bean
+    public ProducerFactory<String, byte[]> producerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                ByteArraySerializer.class);
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+    @Bean
+    public KafkaTemplate<String, byte[]> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
     public ConsumerFactory<String, byte[]> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
@@ -42,12 +66,44 @@ public class KafkaConsumerConfig {
     }
 
     @Bean
+    public DefaultErrorHandler defaultErrorHandler(
+            KafkaTemplate<String, byte[]> kafkaTemplate
+    ) {
+
+        DeadLetterPublishingRecoverer recoverer =
+                new DeadLetterPublishingRecoverer(
+                        kafkaTemplate,
+                        (record, ex) ->
+                                new TopicPartition(
+                                        record.topic() + ".DLT",
+                                        record.partition()
+                                )
+                );
+
+        // retry 3 times with 1 second delay
+        FixedBackOff backOff = new FixedBackOff(1000L, 3);
+
+        return new DefaultErrorHandler(recoverer, backOff);
+    }
+
+
+    @Bean
     public ConcurrentKafkaListenerContainerFactory<String, byte[]>
-    kafkaListenerContainerFactory() {
+    kafkaListenerContainerFactory(DefaultErrorHandler defaultErrorHandler) {
 
         ConcurrentKafkaListenerContainerFactory<String, byte[]> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
+
         factory.setConsumerFactory(consumerFactory());
+
+        // ✅ now this is a method parameter (a variable)
+        factory.setCommonErrorHandler(defaultErrorHandler);
+
+        // 🔥 REQUIRED because you use Acknowledgment in listener
+        factory.getContainerProperties()
+                .setAckMode(ContainerProperties.AckMode.MANUAL);
+
         return factory;
     }
+
 }
--- src/main/java/org/project/notificationservice/services/NotificationEventRouter.java
+++ src/main/java/org/project/notificationservice/services/NotificationEventRouter.java
@@ -4,7 +4,11 @@ import lombok.AllArgsConstructor;
 import org.project.notificationservice.events.NotificationEvent;
 import org.project.notificationservice.handlers.NotificationEventHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
+import tools.jackson.core.exc.StreamReadException;
 import tools.jackson.databind.ObjectMapper;
 
 import java.util.List;
@@ -29,13 +33,27 @@ public class NotificationEventRouter {
             topics = "notification.events",
             groupId = "notification-group"
     )
-    public void consume(byte[] payload) {
+    public void consume(byte[] payload, Acknowledgment acknowledgment,
+                        @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+                        @Header(KafkaHeaders.RECEIVED_PARTITION) int partition) {
         try {
             NotificationEvent event = objectMapper
                     .readValue(payload, NotificationEvent.class);
             route(event);
+            acknowledgment.acknowledge();
+            /**
+             * spring.kafka.consumer.enable-auto-commit=false in application.properties
+             * acknowledgment.acknowledge();
+             * For manual offset management, uncomment the line above and
+             * By this way we control the retry mechanism, if don't want the event published to DLT
+             * Use cases like Poison Message (where the event itself is malformed and cannot be processed like the first catch block)
+             */
+        } catch(StreamReadException e) {
+            acknowledgment.acknowledge();
+            System.out.println("Failed to consume Kafka event. TOPIC:" + topic + ", PARTITION:" + partition + ", " +
+                    "REASON: Invalid Event for notification, " + objectMapper.readValue(payload, String.class));
         } catch (Exception ex) {
-            throw new RuntimeException("Failed to consume Kafka event", ex);
+            throw new RuntimeException("Failed to consume Kafka event. TOPIC:" + topic + ", PARTITION:" + partition , ex);
         }
     }
 }
